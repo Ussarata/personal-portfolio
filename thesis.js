5d25cd0e2e09a8a42a5fe9fad997a1aa3c5b0c59
Ussama <umustafa23@wooster.edu>
2023-07-21 18:06:35 -0400

    almost done

---
+++ thesis.js
@@ -0,0 +1,6 @@
+function downloadthesis() {
+    var link = document.createElement('a');
+    link.href = './Senior_Thesis - Ussama Mustafa.pdf';
+    link.download = 'Thesis - Ussama Mustafa.pdf';
+    link.click();
+}
